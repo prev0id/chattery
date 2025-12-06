4d2e99b2eff5cc3ce2d11fad1fadd05424f767d6
Deev Semen Alekseevich <sdeev@ozon.ru>
2025-12-06 21:08:46 +0300

    add missed not null

--- backend/user_service/migrations/00002_create_images_table.sql
+++ backend/user_service/migrations/00002_create_images_table.sql
@@ -2,9 +2,9 @@
 -- +goose StatementBegin
 CREATE TABLE images (
     id          BIGSERIAL PRIMARY KEY,
-    user_id     BIGINT NOT NULL,
-    image_id    TEXT NOT NULL,
-    file_type   TEXT,
+    user_id     BIGINT    NOT NULL,
+    image_id    TEXT      NOT NULL,
+    file_type   TEXT      NOT NULL,
     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMP NOT NULL DEFAULT NOW()
 );
